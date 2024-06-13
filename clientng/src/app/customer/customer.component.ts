a82fe5ad21a31e6b00cc5e6dbeb5d5977a35e574
Rishi Raj <rishiraj88+github@gmail.com>
2024-06-13 23:20:59 +0200

    Push WIP code for visibility to peers.

--- clientng/src/app/customer/customer.component.ts
+++ clientng/src/app/customer/customer.component.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import { Component, EventEmitter, Output } from '@angular/core';
+import { CustomerDTO } from './dto/CustomerDTO';
 
 @Component({
   selector: 'app-customer',
@@ -8,5 +9,32 @@ import { Component } from '@angular/core';
   styleUrl: './customer.component.css'
 })
 export class CustomerComponent {
+  private customerDTO?: CustomerDTO
+  private url = 'http://localhost:8080/customer/'
+
+  constructor(private customerService: CustomerService) { }
+
+  save(customer: CustomerDTO) {
+    this.customerDTO = this.customerService.save(customer)
+  }
+
+  //default values added for quick review post app installation
+  name: string = 'Alex'
+  birthDate: Date = new Date("11.11.2005"); //11 Nov 2005
+
+  saveEditLabel: string = 'Save'
+
+  @Output()
+  customerDtoEvent: EventEmitter<CustomerDTO> = new EventEmitter<CustomerDTO>();
+
+  prepareCustomerDTO() {
+    console.log(this.name)
+    console.log(this.birthDate)
+    let customerDTO = new CustomerDTO(this.name, this.birthDate)
+    this.customerDtoEvent.emit(customerDTO)
+    console.log(customerDTO)
+    this.saveEditLabel= 'Edit'
+  }
+
 
 }
